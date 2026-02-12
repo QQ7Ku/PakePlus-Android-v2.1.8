6765f7f9e558f0743e34e27624f46c7f7267e9d3
QQ7Ku <luoc120@gmail.com>
2026-02-12 12:38:46 +0800

    update src file from pakeplus

---
+++ scripts/www/js/components/index.js
@@ -0,0 +1,12 @@
+/**
+ * Components Index
+ * 组件导出索引
+ */
+
+export { LanguageSwitcher } from './languageSwitcher.js';
+export { 
+  renderDropdownMenu, 
+  createDropdownItem, 
+  updateDropdownSelection,
+  clearDropdown 
+} from './dropdownRenderer.js';
